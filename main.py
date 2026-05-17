1200903977136d833329b2dafebc9fb95faf395b
Moaz Gaballah <moazforfire@gmail.com>
2026-05-17 15:52:13 +0300

    modify output name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse,os
 
 from stage1.LZ77 import lz77_compress
 from stage2.deflate_symboles import covert_to_event
@@ -19,12 +19,11 @@ parser.add_argument('-d', '--decompress', action='store_true',
 args = parser.parse_args()
 
 if args.compress:
-    with open(args.filename, "r") as f:
+    with open(args.filename, "rb") as f:
         data = f.read()
 
-    data_byte = data.encode("utf-8")
 
-    tokens = lz77_compress(data_byte)
+    tokens = lz77_compress(data)
     event_streams = covert_to_event(tokens)
 
     lit_freq, dist_freq = count_frequencies(event_streams)
@@ -33,19 +32,23 @@ if args.compress:
     lit_codes = canonical_huffman(lit_lengths)
     dist_codes = canonical_huffman(dist_lengths)
 
-    with open("data.txt.sdfl", "xb") as f:
-        f.write(compress_stage4(event_streams, lit_codes, dist_codes))
+    compressed_name = args.filename + ".sdfl"
+    compress = compress_stage4(event_streams, lit_codes, dist_codes)
 
-    print(f"Compressed: {args.filename} -> data.txt.sdfl")
+    with open(compressed_name, "wb") as f:
+        f.write(compress)
+
+    print(f"Compressed: {args.filename}|{len(data)} bytes -> {compressed_name}|{len(compress)} bytes")
 
 elif args.decompress:
     with open(args.filename, "rb") as f:
         uncompressed = decompress_stage4(f.read())
 
-    with open("data.txt", "xb") as f:
+    decompressed_name = args.filename.replace(".sdfl", "")
+    with open(decompressed_name, "wb") as f:
         f.write(uncompressed)
         
-    print(f"DeCompressed: {args.filename} → data.txt")
+    print(f"DeCompressed: {args.filename} → {decompressed_name}")
 
 else:
     print("Please specify -c to compress or -d to decompress")
